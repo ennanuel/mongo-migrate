72d0729fe089eee086f51b5a82a711302620f6ea
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-01 13:23:47 +0100

    Stopped using click import

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import click
 from pymongo import MongoClient
 import os
 
@@ -9,15 +8,13 @@ if os.path.exists(env_path):
             key, value = line.strip().split('=')
             os.environ[key] = value
 
-@click.command()
-
 def migrate_data():
     try:
         live_uri = os.environ.get('LIVE_MONGO_URI')
         local_uri = os.environ.get('LOCAL_MONGO_URI')
         database = os.environ.get('DATABASE_NAME')
 
-        click.echo(f'Copying data from {live_uri} to {local_uri} for database {database}')
+        print(f'Copying data from {live_uri} to {local_uri} for database {database}')
 
         live_client = MongoClient(live_uri)
         local_client = MongoClient(local_uri)
@@ -29,23 +26,23 @@ def migrate_data():
 
         for collection_name in collections:
             try:
-                click.echo(f'Clearing data in {collection_name} in the local database...')
+                print(f'Clearing data in {collection_name} in the local database...')
                 local_db[collection_name].delete_many({})
 
-                click.echo(f'Copying data from {collection_name}...')
+                print(f'Copying data from {collection_name}...')
                 collection_data = live_db[collection_name].find()
                 
                 local_db[collection_name].insert_many(collection_data)
 
-                click.echo(f'{collection_name} data copy complete.')
+                print(f'{collection_name} data copy complete.')
 
             except Exception as e:
-                click.echo(f'Error copying data for {collection_name}: {str(e)}')
+                print(f'Error copying data for {collection_name}: {str(e)}')
 
-        click.echo('Data copy complete.')
+        print('Data copy complete.')
 
     except Exception as e:
-        click.echo(f'Error: {str(e)}')
+        print(f'Error: {str(e)}')
 
 if __name__ == '__main__':
     migrate_data()
